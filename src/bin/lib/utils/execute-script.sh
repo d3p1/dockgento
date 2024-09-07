1db8c561721b8acec8a82a866d6431fb4850be9a
Cristian Marcelo de Picciotto <cmdepicciotto@gmail.com>
2024-09-07 15:37:45 -0300

    refactor: improve comments in execute script util [#1]

--- src/bin/lib/utils/execute-script.sh
+++ src/bin/lib/utils/execute-script.sh
@@ -14,7 +14,7 @@
 # @note   In reality, the `shift` command and
 #         the `@` symbol (as a rest operator) are used to retrieve
 #         script parameters. Consequently, every argument
-#         following the `$1` parameter is treated as part of `$2`, 
+#         following the `$1` parameter (script path) is treated as part of `$2`,
 #         and will be used as a script parameter
 ##
 execute_script() {
@@ -25,8 +25,8 @@ execute_script() {
     # @note It is used "dot space script" calling syntax 
     #       (it is like `source` command), so it is used current shell to
     #       execute the script (and it is not created a sub-shell that avoids
-    #       the definition of environment variable that can be used in 
-    #       main shell script).
+    #       the use/definition of environment variables
+    #       from/to the main shell script).
     #       It is not possible to use the `exec` command because this command
     #       replace main shell script (instead of reusing it), so when the
     #       executed script tries to return to main shell script, it does not
