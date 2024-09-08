25e4a2e922d3afcaa598f5d1bbe057153364132e
Cristian Marcelo de Picciotto <cmdepicciotto@gmail.com>
2024-09-08 16:49:39 -0300

    feat: add dockgento mage-install command [#1]

---
+++ src/bin/lib/mage-install.sh
@@ -0,0 +1,47 @@
+##
+# @description Dockgento command to install a Magento platform
+# @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+##
+
+##
+# @note Import required utilities
+##
+source $BASE_DIR/lib/utils/log.sh
+
+##
+# Main
+#
+# @return void
+##
+main() {
+	_install_magento_platform
+}
+
+##
+# Install Magento platform
+#
+# @return void
+# @note   Install Magento using Composer installation process
+# @note   It is used `docker compose run cli` instead of `docker compose up cli`
+#         to be able to define command to run (it is not possible to do that
+#         using `docker compose up <service>`)
+# @link   https://experienceleague.adobe.com/en/docs/commerce-operations/installation-guide/composer
+##
+_install_magento_platform() {
+	##
+	# @note The `cli` service uses the project PHP CLI image.
+	#       This image has an `init` script that receives as first param
+	#       a flag to determine if it is required to execute an installation.
+	#       If it is set to `true`, then a Magento platform installation is
+	#       executed. If it is set to `false`, then a Magento setup is executed
+	# @link https://hub.docker.com/r/d3p1/magento-php
+	##
+	print_message "[NOTICE] Start Magento installation"
+	docker compose run --rm cli init 1
+	print_message "[NOTICE] End Magento installation"
+}
+
+##
+# @note Call main
+##
+main "$@"
