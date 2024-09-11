a1c2767955dd5bfc21dc457720f597a7c609804f
Cristian Marcelo de Picciotto <cmdepicciotto@gmail.com>
2024-09-10 23:37:03 -0300

    fix: adjust docker installation to avoid errors in first attempt [#1]

--- src/bin/lib/utils/docker/install.sh
+++ src/bin/lib/utils/docker/install.sh
@@ -4,7 +4,6 @@
 # @description An utility to install Docker tools
 # @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
 # @todo        For now, this utility only works with Debian
-# @todo        Analyze why first attempt of installation always fails
 ##
 
 ##
@@ -45,6 +44,7 @@ _configure_docker_repo() {
     sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
 }
 
 ##
