29fca55ae7724bff3140d3f49852a228f065aa9f
C. M. de Picciotto <d3p1@d3p1.dev>
2026-02-08 23:41:27 +0000

    feat: improve mage configure script so it is copied production dockerfile [#20]

--- src/bin/lib/mage-configure.sh
+++ src/bin/lib/mage-configure.sh
@@ -8,7 +8,6 @@
 ##
 # @note Import required utilities
 ##
-source $BASE_DIR/lib/utils/log.sh
 source $BASE_DIR/lib/utils/execute-command-script.sh
 
 ##
@@ -17,34 +16,7 @@ source $BASE_DIR/lib/utils/execute-command-script.sh
 # @return void
 ##
 main() {
-    ##
-    # @note In order to configure a Magento platform,
-    #       the Redis, MariaDB and other resources should be running,
-    #       to avoid Magento exceptions
-    ##
-    docker compose up -d
-
-    ##
-    # @note Before executing the script that configure a Magento platform,
-    #       it is required to prepare the environment services to meet
-    #       conditions needed for a Magento configuration
-    ##
     _execute_configure_script "configure-services"
-
-    ##
-    # @note The `cli` service uses the project PHP CLI image.
-    #       This image has an `init` script that receives as first param
-    #       a flag to determine if it is required to execute an installation.
-    #       If it is set to `true`, then a Magento platform installation is
-    #       executed. If it is set to `false`, then a Magento set up
-    #       action is executed
-    # @note It is used `docker compose run cli`
-    #       instead of `docker compose up cli`
-    #       to be able to define command to run (it is not possible to do that
-    #       using `docker compose up <service>`)
-    # @link https://hub.docker.com/r/d3p1/magento-php
-    ##
-    docker compose run --rm cli init 0
 }
 
 ##
--- src/bin/lib/mage-configure/configure-services.sh
+++ src/bin/lib/mage-configure/configure-services.sh
@@ -9,6 +9,7 @@
 # @note Import required utilities
 ##
 source $BASE_DIR/lib/utils/log.sh
+source $BASE_DIR/lib/utils/envsubst-files.sh
 source $BASE_DIR/lib/utils/mysql/normalize-dump.sh
 
 ##
@@ -24,10 +25,65 @@ main() {
 # Configure Magento
 #
 # @return void
-# @note   Migrate old platform DB
+# @note   Migrate old platform DB.
+#         It is decided to add this step here instead of in a
+#         development scope, because there are situations where
+#         can be useful to migrate a DB to a production environment.
+#         For instance, when it is migrated between production infrastructures,
+#         or when a platform is released and it is used the development DB
+#         as seed
+# @note   Add `Dockerfile` required for production environments.
+#         It is decided to add this step here instead of in a
+#         production scope, because this is something related to the
+#         platform source code and not to the production environment itself.
+#         Additionally, this step is executed here
+#         and not with `dockgento init`,
+#         because Magento platform should be already present and
+#         this is only guarantee with this `dockgento mage-configure` command
 ##
 _configure_magento() {
     print_message "Start Magento configuration" "notice"
+
+    ##
+    # @note Configure Magento DB if DB dump exits
+    ##
+    _configure_magento_db
+
+    ##
+    # @note Copy `Dockerfile` to app so it is possible
+    #       to build production image
+    # @note Replace `${SCRIPT_*}` variables inside `Dockerfile` with
+    #       current environment data.
+    #       In that way, the `Dockerfile` already has useful default data
+    #       for the production image generation
+    ##
+    cp -R "$BASE_DIR/etc/Dockerfile.prod" "$SCRIPT_DOC_ROOT_DIR/Dockerfile"
+    envsubst_files "$SCRIPT_DOC_ROOT_DIR/Dockerfile" '${SCRIPT_PHP_VERSION}'
+
+    print_message "End Magento configuration" "notice"
+}
+
+##
+# Configure Magento DB
+#
+# @returns void
+##
+_configure_magento_db() {
+    ##
+    # @note Check if DB dump location is given to init DB migration
+    ##
+    if [ -z "$SCRIPT_DB_DUMP" ]; then
+        return 0
+    fi
+
+    print_message "Start Magento DB configuration" "notice"
+
+    ##
+    # @note In order to configure a Magento DB platform,
+    #       MariaDB and other resources should be running,
+    #       to avoid Magento exceptions
+    ##
+    docker compose up -d
 
     ##
     # @note The DB dump is normalized to avoid user permission errors
@@ -42,7 +98,8 @@ _configure_magento() {
     #       using service commands
     # @note It is executed the DB dump migration command within a new
     #       container shell to be able to use container environment variables
-    # @todo For now, to make sure that the `mariadb` service and its server is running
+    # @todo For now, to make sure that the `mariadb` service
+    #       and its server is running
     #       the script sleeps for 5 seconds. Improve this logic
     # @link https://superuser.com/questions/1628497/docker-exec-with-dollar-variable
     ##
@@ -53,7 +110,25 @@ _configure_magento() {
     'mariadb -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" < /tmp/db.sql'
     print_message "End DB deploy" "notice"
 
-    print_message "End Magento configuration" "notice"
+    ##
+    # @note The `cli` service uses the project PHP CLI image.
+    #       This image has an `init` script that receives as first param
+    #       a flag to determine if it is required to execute an installation.
+    #       If it is set to `true` (`1`),
+    #       then a Magento platform installation is
+    #       executed. If it is set to `false` (`0`), then a Magento set up
+    #       action is executed.
+    #       In this case, it is executed a Magento set up so the new
+    #       DB is configured to use our environment services
+    # @note It is used `docker compose run cli`
+    #       instead of `docker compose up cli`
+    #       to be able to define command to run (it is not possible to do that
+    #       using `docker compose up <service>`)
+    # @link https://hub.docker.com/r/d3p1/magento-php
+    ##
+    docker compose run --rm cli init 0
+
+    print_message "End Magento DB configuration" "notice"
 }
 
 ##
